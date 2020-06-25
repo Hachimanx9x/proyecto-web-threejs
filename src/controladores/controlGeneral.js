f49862d606197c4f4516b3123dbdab9416ce7f03
Hachiman999 <30505008+Hachiman999@users.noreply.github.com>
2020-06-25 17:41:50 -0500

    se agrego un cubos para medir la interaccion

--- src/controladores/controlGeneral.js
+++ src/controladores/controlGeneral.js
@@ -6,5 +6,14 @@ const controlador ={
 controlador.index = (req, res)=>{
     res.render('index'); 
 }
+controlador.entorno = (req, res)=>{
+    res.render('entorno'); 
+}
+controlador.interaccion = (req, res)=>{
+    res.render('interaccion'); 
+}
+controlador.controles = (req, res)=>{
+    res.render('controlCamara'); 
+}
 
 module.exports = controlador; 
--- src/rutas/index.js
+++ src/rutas/index.js
@@ -5,6 +5,9 @@ const controlGeneral = require('../controladores/controlGeneral');
 
 module.exports =app=>{
     rutas.get('/', controlGeneral.index); 
+    rutas.get('/entorno', controlGeneral.entorno); 
+    rutas.get('/interaccion', controlGeneral.interaccion); 
+    rutas.get('/controlCamara', controlGeneral.controles); 
 
     app.use(rutas); 
 }
