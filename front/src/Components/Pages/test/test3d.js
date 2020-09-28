5682d2e5e050e880330d2662b8128e64219eac87
Hachiman999 <30505008+Hachiman999@users.noreply.github.com>
2020-09-27 21:18:11 -0500

    holi 3d

--- front/src/Components/Pages/test/test3d.js
+++ front/src/Components/Pages/test/test3d.js
@@ -11,8 +11,20 @@ class Test3d extends Component {
     }
 
     componentDidMount(){
-        if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
-        this.init(); 
+        
+        
+        this.container = document.getElementsByClassName("div3d");;
+       
+        this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+        this.controls = new OrbitControls(  this.camera );
+        this.scene = new THREE.Scene();
+        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+        this.light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
+         // scene size
+         this.WIDTH = window.innerWidth;
+         this.HEIGHT = window.innerHeight;
+
+         this.init(); 
     }
     
 
@@ -20,13 +32,13 @@ class Test3d extends Component {
 
        // container = document.createElement( 'div' );
         //document.body.appendChild( container );
-        container = document.getElementsByClassName("div3d");
-        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-        camera.position.set( -1.8, 0.9, 2.7 );
+       
+        
+        this.camera.position.set( -1.8, 0.9, 2.7 );
 
-        controls = new OrbitControls( camera );
-        controls.target.set( 0, 0, 0 );
-        controls.update();
+       
+        this.controls.target.set( 0, 0, 0 );
+        this.controls.update();
 
         // skybox
         var path = '../../../logos/img/Bridge2/';
@@ -38,11 +50,11 @@ class Test3d extends Component {
         ] );             
   
 
-        scene = new THREE.Scene();
-        scene.background = envMap ;
-        light = new THREE.HemisphereLight( 0xbbbbff, 0x444422 );
-        light.position.set( 0, 1, 0 );
-        scene.add( light );
+        
+        this.scene.background = envMap ;
+        
+       this.light.position.set( 0, 1, 0 );
+       this.scene.add( this.light );
 
 //--------------------------------------------------------------------------------------
 const objmesa = new THREE.Object3D(); 
@@ -65,32 +77,30 @@ mesa.load(urlmesa, (gltf)=>{
     //escala
     objmesa.scale.x= 1;  objmesa.scale.y= 1; objmesa.scale.z= 1; 
     //insertar en la escena
-    scene.add(objmesa); 
+    this.scene.add(objmesa); 
 });
 
 
-const cargarModelos = new CargarObjGltf(); 
-cargarModelos.insertarEscena(envMap,new GLTFLoader(), new THREE.Object3D(), 
- '/publico/modelos/Mesa.gltf',//url del modelo
- scene,
-1,-0.3,-1.75, //x y z posicion
-0,0,0,//x y z rotacion
-1,1,1//x y z escala
-    );
-   
+ 
     
-    
-    /*
-cargarModelos.insertarEscena(envMap,new THREE.GLTFLoader(), new THREE.Object3D(),   '/publico/modelos/floor.gltf',scene, -2.72,-0.3,-1.75, 0,0,0, 1,1,1); 
-cargarModelos.insertarEscena(envMap,new THREE.GLTFLoader(), new THREE.Object3D(),   '/publico/modelos/lights.gltf',scene, 1,-0.3,-1.75, 0,0,0, 1,1,1); 
-cargarModelos.insertarEscena(envMap,new THREE.GLTFLoader(), new THREE.Object3D(),   '/publico/modelos/mueble_2.gltf',scene, -0.3,-0.1 ,-0.7, 0,0,0, 1,1,1); 
-cargarModelos.insertarEscena(envMap,new THREE.GLTFLoader(), new THREE.Object3D(),   '/publico/modelos/paredes_1.gltf',scene, 1,-0.25,-2, 0,0,0, 1,1,1); 
-cargarModelos.insertarEscena(envMap,new THREE.GLTFLoader(), new THREE.Object3D(),   '/publico/modelos/paredes_2.gltf',scene, 1,6.04 ,-2, 0,0,0, 1,1,1); 
-*/
+
 var sillas=[];
 for(var i =0 ; i<8 ; i++){
-    sillas.push(cargarModelos.getOBJ3d(envMap,new GLTFLoader(), new THREE.Object3D(),'../../../logos/modelos3d/silla.gltf' )); 
-    sillas[i].position.y=-0.33;
+   // sillas.push(cargarModelos.getOBJ3d(envMap,new GLTFLoader(), new THREE.Object3D(),'../../../logos/modelos3d/silla.gltf' )); 
+   // sillas[i].position.y=-0.33;
+   new GLTFLoader().load('../../../logos/modelos3d/silla.gltf', (gltf)=>{
+    gltf.scene.traverse((nino)=>{
+        if ( nino.isMesh ) {
+
+            nino.material.envMap = envMap;
+
+        }
+         
+    });
+    sillas.push(new THREE.Object3D().add(gltf.scene)); 
+  
+});
+
 }  
 
 sillas[0].position.z=-1.7; sillas[0].position.x=1.32;
@@ -106,15 +116,15 @@ sillas[7].rotation.y = - Math.PI/2; sillas[7].position.x=2.28;sillas[7].position
 
 
 for(var i =0 ; i<8 ; i++){
-   scene.add(sillas[i]); 
+    this.scene.add(sillas[i]); 
 }
 //----------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------
 var geometry = new THREE.PlaneBufferGeometry( 0.65, 1.93 );
         var verticalMirror = new Reflector( geometry, {
             clipBias: 0.0005,
-            textureWidth: WIDTH * window.devicePixelRatio,
-            textureHeight: HEIGHT * window.devicePixelRatio,
+            textureWidth:  this.WIDTH * window.devicePixelRatio,
+            textureHeight:  this.HEIGHT * window.devicePixelRatio,
             color: 0x889999,
             recursion: 1
         } );
@@ -122,34 +132,34 @@ var geometry = new THREE.PlaneBufferGeometry( 0.65, 1.93 );
         verticalMirror.position.x = 0.023;
         verticalMirror.position.z = - 0.047;
         verticalMirror.rotation.x = -Math.PI/2; 
-        scene.add( verticalMirror );
+        this.scene.add( verticalMirror );
 
 var objectopasidad = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { color: 0x0000000, opacity: 0.5 , transparent: true } ) );
 objectopasidad.position.y = verticalMirror.position.y+0.001; 
 objectopasidad.position.x = verticalMirror.position.x ;
 objectopasidad.position.z = verticalMirror.position.z;
 objectopasidad.rotation.x = verticalMirror.rotation.x ;
-    scene.add(objectopasidad)
+    this.scene.add(objectopasidad)
 //-----------------------------------------------------------------------------------------
 
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        renderer.gammaOutput = true;
-        container.appendChild( renderer.domElement );
 
-        window.addEventListener( 'resize', onWindowResize, false );
+this.renderer.setPixelRatio( window.devicePixelRatio );
+this.renderer.setSize( window.innerWidth, window.innerHeight );
+this.renderer.gammaOutput = true;
+this.container.appendChild(  this.renderer.domElement );
+
+        window.addEventListener( 'resize',  this.onWindowResize, false );
 
        
-    animate();
+        this.animate();
     }
 
     onWindowResize() {
 
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
 
     }
 
@@ -157,11 +167,11 @@ objectopasidad.rotation.x = verticalMirror.rotation.x ;
 
     animate() {
 
-        requestAnimationFrame( animate );
+        requestAnimationFrame(  this.animate );
 
-        renderer.render( scene, camera );
+        this.renderer.render(  this.scene,  this.camera );
 
-        stats.update();
+       
 
     }
  render(){
