be3ef4e2c7a072fd49751f6d6f1bf7dd80c7ce1c
Hachiman999 <30505008+Hachiman999@users.noreply.github.com>
2020-09-27 20:21:25 -0500

    se agrego el chat :D

--- Back/src/server/socket.js
+++ Back/src/server/socket.js
@@ -2,11 +2,13 @@ const http = require('./httpx');
 const io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('un usuario conectado');
     socket.on('disconnect', function(){
-      console.log('user disconnected');
+      console.log('un usuario desconectado');
     });
     socket.on('chat message', function(msg){
+      console.log(msg); 
+      msg.from =socket.id.slice(8);  
       io.emit('chat message', msg);
     });
   });
