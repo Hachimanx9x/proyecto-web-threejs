429698c84947a2de9d2e7d2d25029c96b182eadb
Nestor Martinez <nestormartinez19999@gmail.com>
2021-01-15 01:16:55 -0500

    Card Member added and project creation page added

--- front/src/Components/Pages/Desktop/Desktop.js
+++ front/src/Components/Pages/Desktop/Desktop.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Desktop.css";
 import CardDesktop from "../../Elements/CardDesktop/CardDesktop";
+//import {Carousel, Item} from 'react-elastic-carousel'
 
 class Desktop extends Component {
     constructor(props) {
@@ -31,8 +32,16 @@ class Desktop extends Component {
                     </div>
                 </div>
                 <div className="col-12 col-sm-3 ml-1 o-blue-container" >
-                        
-
+                  {/** 
+                   *   <Carousel itemsToShow={1}>
+                        <Item>1</Item>
+                        <Item>2</Item>
+                        <Item>3</Item>
+                        <Item>4</Item>
+                        <Item>5</Item>
+                        <Item>6</Item>
+                    </Carousel>
+                  */}
                 </div>
             </div>
         );
--- front/src/Components/Pages/Projects/CreateProject.js
+++ front/src/Components/Pages/Projects/CreateProject.js
@@ -1,28 +1,47 @@
-import React from "react";
-import TextField from '@material-ui/core/TextField';
+import React, {Component} from "react";
 import "./CreateProject.css";
+import { MDBInput } from "mdbreact";
 
-const CreateProject = () => {
-    return (<div>
-        <h4>Hi</h4>
-        <TextField
-            id="name"
-            label="Name"
-            variant="filled"
-            margin="normal"
-            className="o-test-textbox"
-            InputLabelProps={{
-                shrink: true,
-            }}
-        />
-        <TextField
-            id="standard-number"
-            label="Number"
-            type="number"
-            margin="normal"
-        />
-    </div>
-    );
+class CreateProject extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { projectIcon: null, projectPicture:null, projectName: "", projectDescription:"", projectMembers:[],projectPractices:""}
+    }
+    render() {
+        return (
+            <div className="row" >
+                <div className="col-12 col-xs-12 col-sm-8 o-project-creation-section">
+                    <h4 className="mb-3 pl-4">Nombre del projecto</h4>
+                    <div className="row bg-white rounded o-project-basic-info">
+                        <div className="col-xs-12 col-sm-4">
+                            <small>Icono del proyecto</small>
+                            <div className="inputWrapper rounded-pill">
+                                <p className="cyan-text" style={{margin:"0.7rem 0.1rem 0rem 1.3rem", fontSize:"1rem", position:"absolute"}}>Subir icono</p> 
+                                <input className="fileInput rounded-pill" type="file" name="file1"/>
+                            </div>
 
-};
+                        </div>
+                        <div className="col-xs-12 col-sm-4">
+                            <MDBInput type="text" label="Nombre del proyecto" outline/>
+                        </div>
+                        <div className="col-xs-12 col-sm-4">
+                            <MDBInput type="textarea" label="Descripción del proyecto" className="rounded" outline/>
+                        </div>
+                    </div>
+                    <div className="bg-white rounded">
+                        <h6 className="mb-3 pl-4">Integratnes del proyecto</h6>
+                        <div className="o-member-selection-container">
+
+                        </div>
+                    </div>
+                   
+                </div>
+                <div className="col-12 col-xs-12 col-sm-4">
+
+                </div>
+            </div>
+        );
+    }
+}
+
 export default CreateProject;
