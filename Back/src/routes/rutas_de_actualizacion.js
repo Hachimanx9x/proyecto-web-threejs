291778b20f751689a0b9af7eb5b488ff11257a56
Hachimanx9x <diego_fer.bolanos@uao.edu.co>
2021-04-22 22:51:41 -0500

    f

--- Back/src/routes/rutas_de_actualizacion.js
+++ Back/src/routes/rutas_de_actualizacion.js
@@ -132,8 +132,55 @@ rutas.put("/comiquieras/actividad", proToken, (req, res) => {
                 console.log("abajo");
               });
           } else {
-            console.log(chalk.red("error de datos"));
-            res.json({ msj: "datos erroneos" });
+            if (req.files !== undefined && req.files !== null) {
+              if (
+                req.files.archivo !== undefined &&
+                req.files.archivo !== null
+              ) {
+                const { archivo } = req.files;
+                console.log(chalk.green(`Entro el archivo ${archivo.name}`));
+                console.log(chalk.green(`La fecha se cambio por ${tecnica}`));
+                archivo.mv(__dirname + "/tmp/" + archivo.name, (err) => {
+                  if (!err) {
+                    var metaData = {
+                      "Content-Type": `${archivo.mimetype}`,
+                      size: archivo.size,
+                      "X-Amz-Meta-Testing": 1234,
+                      example: 5678,
+                    };
+                    console.log(
+                      chalk.bgGreen("|   |") + " actualizando actividad"
+                    );
+                    actualizarDB
+                      .entregaractividad(actividad, archivo.name)
+                      .then((result) => {
+                        console.log(
+                          chalk.bgGreen("|   |") +
+                            `insertando en el bocket ${result.proyecto}`
+                        );
+                        ftpminio
+                          .putFile(
+                            `proyecto${result.proyecto}`,
+                            archivo.name,
+                            path.join(__dirname, `/tmp/${archivo.name}`),
+                            metaData
+                          )
+                          .then((result2) => {
+                            res.json({
+                              file: `${env.host}/proyecto/contenido/proyecto${result.proyecto}/${archivo.name}`,
+                            });
+                          })
+                          .catch((err2) => res.json(err2));
+                      })
+                      .catch((err) => res.json(err));
+                  } else {
+                    console.log(err);
+                  }
+                });
+              }
+            } else {
+              res.json({ msj: "no se envio nada a actualizar" });
+            }
           }
         })
         .catch((errtec) => res.json(errtec));
